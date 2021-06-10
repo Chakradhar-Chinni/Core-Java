bd30bb5b20e287d0c167ca0b3c3a342200309882
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-10 23:27:44 +0530

    Update Innerdemo.java

--- Archives/Innerdemo.java
+++ Archives/Innerdemo.java
@@ -1,7 +1,7 @@
 package com.youtube.learning;
 
 class Outer  //outer class 
-{
+{ 
 	public void show() //Method inside outer class    
 	{         
 	System.out.println("From Outer class 1.1");       
