f2a051cd18e9a784278f108c015890dfa37a6f26
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-10 23:23:33 +0530

    Update Innerdemo.java

--- Archives/Innerdemo.java
+++ Archives/Innerdemo.java
@@ -6,7 +6,7 @@ class Outer  //outer class
 	{
 	System.out.println("From Outer class 1.1");
 	}
-	 class Inner //class inside a class
+	 class Inner //class inside a class 
 	{
 		public void show1()
 		{
