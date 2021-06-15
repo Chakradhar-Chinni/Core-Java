d787eb10c5fc19584c12c2362af8dc9e1ec1776f
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-15 09:44:28 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		//System.out.println("Hello World");
+		ArrayList <String> l = new ArrayList<String>();
+		l.add("A");
+		l.add("B");
+		l.add("E");
+		l.add("R");
+		l.add("C");
+		l.add("D");
+    
+    System.out.println(l);
+    
+	}
+}
