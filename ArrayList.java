30b7f25f2917dd1e64b3ef21b87dfb2b72f4972f
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-06-15 09:46:00 +0530

    Using Iterator in ArrayList

--- ArrayList.java
+++ ArrayList.java
@@ -16,3 +16,29 @@ public class Main
     
 	}
 }
+
+
+===================
+      Iterator
+===================
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		//System.out.println("Hello World");
+		ArrayList <String> l = new ArrayList<String>();
+		l.add("A");
+		l.add("B");
+		l.add("E");
+		l.add("R");
+		l.add("C");
+		l.add("D");
+		Iterator itr = l.iterator();
+		while(itr.hasNext())
+		{
+		    System.out.println(itr.next()+"9");
+		}
+	}
+}
