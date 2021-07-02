8e2392950dbc5cb4bad5f6622b2b19c0e179e4a3
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-07-02 15:08:51 +0530

    Create App.java

---
+++ Interface & class in eclispe/App.java
@@ -0,0 +1,16 @@
+package com.youtube.telusko.SpringDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       // System.out.println( "Hello World!" );
+        
+        	Vehicle obj = new Car();
+        	obj.run();
+    }
+}
