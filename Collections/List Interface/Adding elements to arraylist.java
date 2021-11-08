3c206eeb988fa4b91843e16d966e9725e6015d2f
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-11-08 23:14:22 +0530

    Create Adding elements to arraylist.java

---
+++ Collections/List Interface/Adding elements to arraylist.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(10);
+        list.add(40);
+        list.add(450);
+        System.out.println(list);  // [10, 20, 30, 10, 40, 450]
+    }
+}
