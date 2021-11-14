d4073b5b6a7447efcb9ada29f9f1a45267d8891a
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-11-14 20:09:21 +0530

    adding String elements at specific Index

--- Collections/List Interface/Useful methods in ArrayList.java
+++ Collections/List Interface/Useful methods in ArrayList.java
@@ -46,3 +46,20 @@ class codechef
 }
 Output: [10, 100, 20, 30]
 //=======================
+
+// adding elements at specific Index
+import java.util.*;
+class codechef
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("A");
+        al.add("B");
+        al.add("C");
+        al.add(0,"Z");
+        System.out.print(al);
+    }
+}
+Output: [Z, A, B, C]
+//=======================
