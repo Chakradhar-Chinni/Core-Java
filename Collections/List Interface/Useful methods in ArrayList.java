c82aa280334cca92db891d9b827ddf06d58e1a28
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-11-14 23:01:14 +0530

    isEmpty()

--- Collections/List Interface/Useful methods in ArrayList.java
+++ Collections/List Interface/Useful methods in ArrayList.java
@@ -154,4 +154,22 @@ class codechef
 }
 Output: 10
 //======================
+
+//checking if arraylist is empty or not | returns true if arraylist is empty else false
+import java.util.*;
+class codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList al1 = new ArrayList<Integer>();
+        al1.add(10);
+        al1.add(20);
+        al1.add(50);
+        System.out.println(al1.isEmpty());
+    }
+}
+Output: false
+
+//======================
     
