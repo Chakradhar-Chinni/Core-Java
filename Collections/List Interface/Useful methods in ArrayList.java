d6abe7200cc2f8eea57adebd55a50c7c869f2341
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-11-14 23:05:33 +0530

    lastIndexOf()

--- Collections/List Interface/Useful methods in ArrayList.java
+++ Collections/List Interface/Useful methods in ArrayList.java
@@ -172,4 +172,25 @@ class codechef
 Output: false
 
 //======================
-    
+
+//finding index of last occurence of element | returns -1 if element is not present
+import java.util.*;
+class codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList al1 = new ArrayList<Integer>();
+        al1.add(10);
+        al1.add(20);
+        al1.add(50);
+        al1.add(20);
+        al1.add(98);
+        al1.add(87);
+        System.out.println(al1.lastIndexOf(20));
+        System.out.println(al1.lastIndexOf(93));
+    }
+}
+Output: 
+3  // element 20 last occured at index 3
+-1  // 93 is not present in list so -1
