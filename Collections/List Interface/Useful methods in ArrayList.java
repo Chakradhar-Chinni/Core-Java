df209347bf567713d820fcde11b7637057c09bd0
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-11-14 20:04:31 +0530

    adding integer elements to Integer Generic

---
+++ Collections/List Interface/Useful methods in ArrayList.java
@@ -0,0 +1,15 @@
+//adding elements using add method into Integer Generic
+import java.util.*;
+class codechef
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        System.out.print(al); 
+    }
+}
+Output: [10, 20, 30]
+//=======================
