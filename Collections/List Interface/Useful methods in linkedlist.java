5b253cb30c79d608f00d0c5565b59675a5caeefe
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-11-15 22:45:56 +0530

    addAll() | merging two linked lists

--- Collections/List Interface/Useful methods in linkedlist.java
+++ Collections/List Interface/Useful methods in linkedlist.java
@@ -21,3 +21,31 @@ Output:
 [10, 20, 30, 40, 50]
 [100, 10, 20, 30, 40, 50]
 true
+
+
+//merging two linked lists
+import java.util.*;
+class codechef
+{
+    public static void main(String[] args)
+    {
+        LinkedList<Integer> list1 = new LinkedList<Integer>();
+        LinkedList<Integer> list2 = new LinkedList<Integer>();
+        
+        list1.add(10);
+        list1.add(20);
+        list1.add(30);
+        list1.add(40);
+                        
+        list2.add(100);
+        list2.add(200);
+        list2.add(300);
+        list2.add(400);
+        System.out.println(list1);
+        System.out.println(list2);
+        list1.addAll(list2);  //list2 will be appended at end of list1
+        System.out.println(list1);
+    }
+}
+
+
