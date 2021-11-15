24e9d653848b628ed1bfa9f6691042ab7d44f501
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-11-15 22:49:47 +0530

    Update Useful methods in linkedlist.java

--- Collections/List Interface/Useful methods in linkedlist.java
+++ Collections/List Interface/Useful methods in linkedlist.java
@@ -49,32 +49,3 @@ class codechef
 }
 
 //merging lists based on index
-import java.util.*;
-class codechef
-{
-    public static void main(String[] args)
-    {
-        LinkedList<Integer> list1 = new LinkedList<Integer>();
-        LinkedList<Integer> list2 = new LinkedList<Integer>();
-        
-        list1.add(10);
-        list1.add(20);
-        list1.add(30);
-        list1.add(40);
-                        
-        list2.add(100);
-        list2.add(200);
-        list2.add(300);
-        list2.add(400);
-        
-        System.out.println(list1);
-        System.out.println(list2);
-        
-        list1.addAll(1,list2);
-        System.out.println(list1);
-    }
-}
-Output:
-[10, 20, 30, 40]
-[100, 200, 300, 400]
-[10, 100, 200, 300, 400, 20, 30, 40]
