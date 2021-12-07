47337f7f21dbb7ff790afe89282d82ce872eef9b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 15:23:25 +0530

    .size() | .clear()

--- Collections/Set Interface/Various methods in HashSet.java
+++ Collections/Set Interface/Various methods in HashSet.java
@@ -14,3 +14,25 @@ class Main
 
 Output:
 [23, 10]
+
+
+//size of HashSet , removing all items from HashSet
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(10);
+        set.add(23);
+        
+        System.out.println(set.size());
+        
+        set.clear();
+        System.out.println(set.size());
+    }
+}
+
+Output:
+2
+0
