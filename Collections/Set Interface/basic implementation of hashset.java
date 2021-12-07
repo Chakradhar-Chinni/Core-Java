7fd8ce6383ea8b19580b64dcd730a806f8552c97
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 15:16:18 +0530

    Create basic implementation of hashset.java

---
+++ Collections/Set Interface/basic implementation of hashset.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(10);
+        set.add(23);
+        
+        System.out.println(set);
+    }
+}
+
+Output:
+[23, 10]
