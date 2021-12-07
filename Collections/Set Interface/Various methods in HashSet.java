13ff6658eaf17f5530936581d7d9575a7152a5ca
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 15:29:44 +0530

    .contains() | .remove()

--- Collections/Set Interface/Various methods in HashSet.java
+++ Collections/Set Interface/Various methods in HashSet.java
@@ -36,3 +36,28 @@ class Main
 Output:
 2
 0
+
+ //contains(), remove()
+ import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(10);
+        set.add(23);
+        set.add(43);
+        set.add(54);
+        set.add(439);
+        boolean r = set.contains(43);  // contains() returns boolean
+        System.out.println(r);
+        
+        if(set.contains(23))
+          set.remove(23);
+        
+        System.out.println(set);
+    }
+}
+Output:
+true
+[54, 439, 10, 43]
