dd64f2058e22b417e9fefe4745dd065e29db157b
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-07 15:21:17 +0530

    //adding elements :: .add()

---
+++ Collections/Set Interface/Various methods in HashSet.java
@@ -0,0 +1,16 @@
+//adding elements :: .add()
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(10);
+        set.add(23);
+        
+        System.out.println(set);
+    }
+}
+
+Output:
+[23, 10]
