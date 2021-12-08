678146e1a02670e354f37f20d365d4df79170d39
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-08 23:27:42 +0530

    Inserting entries into map <K,V>

---
+++ Collections/Map Interface/Various methods in HashMap.java
@@ -0,0 +1,17 @@
+//Inserting entries into map
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        map.put(1001,10);
+        map.put(1002,20);
+        
+        System.out.print(map);
+    }
+}
+/*
+Output:
+{1001=10, 1002=20}
+*/
