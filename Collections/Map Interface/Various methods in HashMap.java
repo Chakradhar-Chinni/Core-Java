1094decc1b1e112e329af9f2618fce3455006754
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-08 23:31:20 +0530

    Removing entry using Key

--- Collections/Map Interface/Various methods in HashMap.java
+++ Collections/Map Interface/Various methods in HashMap.java
@@ -15,3 +15,27 @@ class Main
 Output:
 {1001=10, 1002=20}
 */
+
+// Removing entry using Key
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        map.put(1001,10);
+        map.put(1002,20);
+        map.put(1003,30);
+        map.put(1004,40);
+        System.out.println("Actual Map: "+ map);
+        
+        map.remove(1003);
+        
+        System.out.println("After removing: "+ map);
+    }
+}
+/*
+Output:
+Actual Map: {1001=10, 1002=20, 1003=30, 1004=40}
+After removing: {1001=10, 1002=20, 1004=40}
+*/
