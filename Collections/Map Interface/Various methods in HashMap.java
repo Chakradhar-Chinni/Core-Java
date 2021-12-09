754c18bdb584f26c5d62eea4268493284cf6d0a6
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2021-12-09 08:55:08 +0530

    Update Various methods in HashMap.java

--- Collections/Map Interface/Various methods in HashMap.java
+++ Collections/Map Interface/Various methods in HashMap.java
@@ -39,3 +39,26 @@ Output:
 Actual Map: {1001=10, 1002=20, 1003=30, 1004=40}
 After removing: {1001=10, 1002=20, 1004=40}
 */
+
+//Fetching value using key | size of HashMap
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        map.put(1001,10);
+        map.put(1002,20);
+        
+        int size = map.size();
+        int value = map.get(1001);
+        
+        System.out.println("HashMap size is: "+size);
+        System.out.println(value);
+    }
+}
+/*
+Output:
+HashMap size is: 2
+10
+*/
